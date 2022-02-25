87633887c30cd859e3ac61a52937c068c78df535
John Hennig <jhen@gmx.net>
2022-02-25 18:10:29 +0100

    Initial release: 0.9.0.

--- pyflake8/meta.py
+++ pyflake8/meta.py
@@ -1,7 +1,7 @@
 ﻿"""Meta information about the package."""
 
 title     = 'pyFlake8'
-synopsis  = 'Hooks into Flake8 to support configuration via `pyproject.toml`.'
+synopsis  = 'Runs Flake8 with configuration from pyproject.toml.'
 version   = '0.9.0'
 author    = 'John Hennig'
 license   = 'MIT'
