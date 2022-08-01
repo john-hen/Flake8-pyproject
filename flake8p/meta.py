1da4649dfc59acc65fb0cee8067d9124c0f9a123
John Hennig <jhen@gmx.net>
2022-08-01 12:28:07 +0200

    Bumped version to 1.0.0.

--- flake8p/meta.py
+++ flake8p/meta.py
@@ -2,6 +2,6 @@
 
 title     = 'Flake8-pyproject'
 synopsis  = 'Runs Flake8 with configuration from pyproject.toml.'
-version   = '0.9.1'
+version   = '1.0.0'
 author    = 'John Hennig'
 license   = 'MIT'
