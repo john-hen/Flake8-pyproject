f16bdf03ce18bd3f7ed3c8d936cea5c2b394b979
Victor Calvert <calvertvl@gmail.com>
2022-08-08 15:59:14 -0400

    More qualtiy gripes plus some documentation.

--- flake8p/plugin.py
+++ flake8p/plugin.py
@@ -1,6 +1,14 @@
+from typing import List
+
 from .util import monkeypatch
 
 class FlakeConfigToml:
+    # pylint: disable=no-self-use
+    """
+    Flake8 plugin to load a `pyproject.toml` file from the current directory.
+    """
+    # Pylint warning disabled because this is a plugin and these methods
+    # need to be present...but we don't need to use `self`.
     def __init__(self, tree):
         pass
 
