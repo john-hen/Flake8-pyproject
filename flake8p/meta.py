7eddc83bab2ddb6b16def09826e70f0258f5659c
John Hennig <jhen@gmx.net>
2022-08-13 09:26:31 +0200

    Updated short description in PyPI metadata.

--- flake8p/meta.py
+++ flake8p/meta.py
@@ -1,7 +1,7 @@
 ﻿"""Meta information about the package."""
 
 title     = 'Flake8-pyproject'
-synopsis  = 'Runs Flake8 with configuration from pyproject.toml.'
+synopsis  = 'Flake8 plug-in loading the configuration from pyproject.toml'
 version   = '1.1.0'
 author    = 'John Hennig'
 license   = 'MIT'
