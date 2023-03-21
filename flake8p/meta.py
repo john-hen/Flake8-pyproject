30b8444781d16edd54c11df08210a7c8fb79258d
John Hennig <jhen@gmx.net>
2023-03-21 21:11:08 +0100

    Bumped version to 1.2.3.

--- flake8p/meta.py
+++ flake8p/meta.py
@@ -2,6 +2,6 @@
 
 title     = 'Flake8-pyproject'
 synopsis  = 'Flake8 plug-in loading the configuration from pyproject.toml'
-version   = '1.2.2'
+version   = '1.2.3'
 author    = 'John Hennig'
 license   = 'MIT'
